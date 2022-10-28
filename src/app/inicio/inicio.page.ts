72ea17fea1d65fee26416aa04eadf5d87d219f23
ChristopherLozano <tic-270014@utnay.edu.mx>
2022-10-28 15:41:04 -0600

    Agregar cambios

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -19,10 +19,28 @@ export class InicioPage implements OnInit {
       redirectTo: '/alert'
     },
   ];
-  constructor() { }
 
   ngOnInit() {
   }
+
+  items = [];  
+  numTimesLeft = 5;  
+  constructor() {  
+    this.addMoreItems();  
+  }  
+  loadData(event) {  
+    setTimeout(() => {  
+      console.log('Done');  
+      this.addMoreItems();  
+      this.numTimesLeft -= 1;  
+      event.target.complete();  
+    }, 500);  
+  }  
+  addMoreItems() {  
+    for (let i = 0; i < 20; i++) {  
+      this.items.push(i);  
+    }  
+  }  
 
 }
 
